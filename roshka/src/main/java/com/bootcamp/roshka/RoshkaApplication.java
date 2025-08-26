05d8cfd82cc4c373476201a580a620f2ef840edb
ZenKySSJ <78515792+zenkyssj@users.noreply.github.com>
2025-08-26 10:05:59 -0300

    Proyecto agregado

---
+++ roshka/src/main/java/com/bootcamp/roshka/RoshkaApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.roshka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoshkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoshkaApplication.class, args);
+	}
+
+}
---
+++ roshka/src/test/java/com/bootcamp/roshka/RoshkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.roshka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoshkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
